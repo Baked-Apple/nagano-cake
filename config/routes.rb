dc2a20804d5f539f348e160b1e843098a043c7be
vagrant <vagrant@localhost.localdomain>
2020-07-02 04:53:57 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'home/top'
+  end
+  namespace :public do
+    get 'home/top'
+    get 'home/about'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
