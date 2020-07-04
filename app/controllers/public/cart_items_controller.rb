2913143feeec69567058fcc4b4befed1d75eb5c0
vagrant <vagrant@localhost.localdomain>
2020-07-04 07:18:03 +0000

    update カートのレイアウトを作った

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,21 @@
 class Public::CartItemsController < ApplicationController
+	def index
+		@cart_item = CartItem.new
+		@cart_items = CartItem.all
+	end
+
+	def create
+		cart_item = CartItem.new()
+		cart_item.save
+		redirect_to public_cart_items_path
+	end
+
+	def update
+	end
+
+	def destroy
+		cart_item = CartItem.find(params[:id])
+		cart_item.destroy
+		redirect_to public_cart_items_path
+	end
 end
