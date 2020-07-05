272d3b17bb38ea93958db0b95292ff317db00d3a
vagrant <vagrant@localhost.localdomain>
2020-07-05 06:26:56 +0000

    update　配送一覧作成

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,2 +1,27 @@
 class Public::DeliveriesController < ApplicationController
+	def index
+		@delivery = Delivery.new
+		@deliveries = Delivery.all
+	end
+
+	def create
+		@delivery = Delivery.new(delivery_params)
+		@delivery.save
+		redirect_to public_deliveries_path
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def delivery_params
+		params.require(:delivery).permit(:postal_code, :address, :name)
+	end
 end
