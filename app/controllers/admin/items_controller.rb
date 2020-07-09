2eb5770e5db380459c6c17e91e652cf8608f59dd
Kamitotomi <rockyaaokage@gmail.com>
2020-07-09 21:05:41 +0900

    ロゴと小数点消し

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -30,7 +30,7 @@ class Admin::ItemsController < ApplicationController
 	def update
 		@item = Item.find(params[:id])
     if @item.update(item_params)
-      	redirect_to admin_item_path(@product.id), notice:'商品情報を更新しました'
+      	redirect_to admin_item_path(@item.id), notice:'商品情報を更新しました'
     else
       render 'edit'
     end
