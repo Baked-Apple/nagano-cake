f4887d816e2d4ade7c8e474e8c648b2c37f1c22f
vagrant <vagrant@localhost.localdomain>
2020-07-09 12:05:33 +0000

    modify 注文機能とadmin注文履歴

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,21 +1,22 @@
 class Admin::OrderItemsController < ApplicationController
 
 	def update
-		@order_item = OrderItems.find(params[:id])
-		if @order_item.update(order_item_params)
-			# 製作状況が２＝製作中になったら、注文ステータスを製作中２に
-		 	if @product_status == 2
-		 		@order.update(order_status: 2)
-		 	# 製作状況が３＝製作完了になったら、注文ステータスを発送準備中３に
-		 	elsif @product_status == 3
-		 		@order_item.update(order_status: 3)
-		 	end
-		 	redirect_to admin_order_path(@order.id)
+		@order_item = OrderItem.find(params[:id])
+		@order = @order_item.order
+		@order_item.order_id = @order.id
+		@order_item.update(order_item_params)
+		# 製作状況が２＝製作中になったら、注文ステータスを製作中２に
+		if @order_item.product_status == "making"
+			@order.update(order_status: 2)
+		# 製作状況が３＝製作完了になったら、注文ステータスを発送準備中３に
+		elsif @order_item.product_status == "done"
+			@order.update(order_status: 3)
 		end
+		redirect_to admin_order_path(@order.id)
     end
 
     def order_item_params
-    	params.require(:order_item).permit(:price, :quantity, :product_status)
+    	params.require(:order_item).permit(:product_status)
 	end
 
 end
