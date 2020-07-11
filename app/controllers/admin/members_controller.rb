a96eb113c28cb48064dc043aaa5798c318860ee0
vagrant <vagrant@localhost.localdomain>
2020-07-11 02:19:57 +0000

    [modified]layout

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -25,10 +25,4 @@ class Admin::MembersController < ApplicationController
        params.require(:member).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :phone_number, :email, :leave_status)
     end
 
-    # def create_activation_digest
-    # 	self.activation_token = Member.new_token
-    # 	self.activation_digest = Member.digest(activation_token)
-    # end
-
-
 end
--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -17,8 +17,6 @@ class Public::MembersController < ApplicationController
     end
 
     def confirm
-    	@member = current_member
-    	@member.update(leave_status: true)
     end
 
     def hide
