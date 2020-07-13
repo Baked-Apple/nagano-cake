292e8361c24823db1e3090742a52788f1ad06d6b
vagrant <vagrant@localhost.localdomain>
2020-07-13 07:07:04 +0000

    [modify]ヘッダー/ジャンルメッセージ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,7 +9,7 @@ class Admin::GenresController < ApplicationController
 	def create
 		@genre = Genre.new(genre_params)
 		if @genre.save
-			flash[:notice] = "You have creatad new genre successfully."
+			flash[:notice] = "ジャンルを追加しました"
 			redirect_to admin_genres_path
 		else
 			@genres = Genre.page(params[:page])
