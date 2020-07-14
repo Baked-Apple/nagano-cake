092778057d558df18c386571446deaf71eda041a
vagrant <vagrant@localhost.localdomain>
2020-07-14 02:42:29 +0000

    [add]seed/item

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,51 +16,12 @@ Admin.create(
 Genre.create([{genre: "ケーキ",invalid_status: 0,}, {genre: "プリン",invalid_status: 0,}, {genre: "焼き菓子",invalid_status: 0,}, {genre: "キャンディ",invalid_status: 0,}])
 
 Item.create([
-	{genre_id: 1, name: "ティラミス", introduction: "当店おすすめ", price: 400, sale_status: 1,},
-	{genre_id: 1, name: "モンブラン", introduction: "当店おすすめ", price: 400, sale_status: 1,},
-	{genre_id: 2, name: "焼きプリン", introduction: "当店おすすめ", price: 300, sale_status: 1,},
-	{genre_id: 2, name: "いちごプリン", introduction: "当店おすすめ", price: 300, sale_status: 1,},
-	{genre_id: 3, name: "バタークッキー", introduction: "当店おすすめ", price: 200, sale_status: 1,},
-	{genre_id: 3, name: "チョコチップクッキー", introduction: "当店おすすめ", price: 200, sale_status: 1,},
-	{genre_id: 4, name: "ミルクキャンディ", introduction: "当店おすすめ", price: 200, sale_status: 1,},
-	{genre_id: 4, name: "フルーツキャンディ", introduction: "当店おすすめ", price: 200, sale_status: 1,},
+	{genre_id: 1, name: "ティラミス", introduction: "当店おすすめ", price: 400, sale_status: 1, image: open("./db/images/ティラミス.jpg")},
+	{genre_id: 1, name: "モンブラン", introduction: "当店おすすめ", price: 400, sale_status: 1, image: open("./db/images/モンブラン.jpg")},
+	{genre_id: 2, name: "焼きプリン", introduction: "当店おすすめ", price: 300, sale_status: 1, image: open("./db/images/焼きプリン.jpg")},
+	{genre_id: 2, name: "いちごプリン", introduction: "当店おすすめ", price: 300, sale_status: 1, image: open("./db/images/いちごプリン.jpg")},
+	{genre_id: 3, name: "バタークッキー", introduction: "当店おすすめ", price: 200, sale_status: 1, image: open("./db/images/バタークッキー.jpg")},
+	{genre_id: 3, name: "チョコチップクッキー", introduction: "当店おすすめ", price: 200, sale_status: 1, image: open("./db/images/チョコチップクッキー.jpg")},
+	{genre_id: 4, name: "ミルクキャンディ", introduction: "当店おすすめ", price: 200, sale_status: 1, image: open("./db/images/ミルクキャンディ.jpg")},
+	{genre_id: 4, name: "フルーツキャンディ", introduction: "当店おすすめ", price: 200, sale_status: 1, image: open("./db/images/フルーツキャンディ.jpg")},
 ])
-
-
-# 商品
-# Item.create!(
-# 	[
-# 		{
-# 			genre_id: 1,
-# 			name: "チョコケーキ",
-# 			introduction: "チョコのショートケーキ",
-# 			price: 500,
-# 			sale_status: 1,
-# 			image: open("./db/images/chocolate_cake.jpg")
-# 		},
-# 		{
-# 			genre_id: 2,
-# 			name: "普通のプリン",
-# 			introduction: "シンプルなプリン",
-# 			price: 500,
-# 			sale_status: 1,
-# 			image: open("./db/images/プリン.jpg")
-# 		},
-# 		{
-# 			genre_id: 3,
-# 			name: "クッキーの詰め合わせ",
-# 			introduction: "長野ケーキオリジナル",
-# 			price: 500,
-# 			sale_status: 1,
-# 			image: open("./db/images/cookie_mix.jpg")
-# 		},
-# 		{
-# 			genre_id: 4,
-# 			name: "りんご飴",
-# 			introduction: "長野県産りんごを使った贅沢りんご飴",
-# 			price: 500,
-# 			sale_status: 1,
-# 			image: open("./db/images/apple_candy.jpg")
-# 		}
-# 	]
-# )
